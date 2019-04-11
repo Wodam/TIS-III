5b9333db052d806b130b43c4755ebbbce8fad249
Bruno Marini <bruno.marini1@gmail.com>
2019-04-11 13:48:57 -0300

    Model name [~] {"Question" -> "Questao"}

--- src/back-end/app/controllers/questionController.js
+++ src/back-end/app/controllers/questionController.js
@@ -3,21 +3,21 @@
 const database = require('./../database');
 
 exports.create = async (values) => {
-	return await database.create('Question', values);
+	return await database.create('Questao', values);
 };
 
 exports.retrieve = async (values) => {
-	return await database.retrieve('Question', values);
+	return await database.retrieve('Questao', values);
 };
 
 exports.update = async (id, values) => {
-	return await database.update('Question', id, values);
+	return await database.update('Questao', id, values);
 };
 
 exports.delete = async (values) => {
-	return await database.delete('Question', values);
+	return await database.delete('Questao', values);
 };
 
 exports.list = async (values) => {
-	return await database.list('Question', values);
+	return await database.list('Questao', values);
 };
